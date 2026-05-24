fe37ad0b36799e4095597de39b884e42477819e3
Justin Jiang <jus3472@gmail.com>
2026-05-24 16:38:32 -0500

    Update deployment base path

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/p43">
+    <BrowserRouter basename="/Product-Feedback-Board">
       <AuthProvider>
         <App />
       </AuthProvider>
